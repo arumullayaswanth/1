a16253b2b1c8156f2c9bf65a211491c8ab16dcfe
Arumulla Yaswanth Reddy <164642237+arumullayaswanth@users.noreply.github.com>
2025-06-24 15:20:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+
+
+
+
 const express = require('express');
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
 
+// Add a health check route
+app.get('/status', (req, res) => {
+  console.log('✅ /status endpoint was called');
+  res.json({ status: 'ok' });
+});
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
